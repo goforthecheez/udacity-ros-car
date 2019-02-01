213ed00de27c1844a8cd27d8abe7d7a1a2dfc235
Andrew Garkavyi <andrew.gawy@gmail.com>
2019-01-31 22:28:10 -0800

    DBW follow the line logic added, no throttle and brake
    - has problem with mac simulator - /current_pose is not updated by simulator in time
    - on linux seem to be functioning nominal

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,24 @@
+from yaw_controller import YawController
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+MIN_SPEED = 0.1
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
-        pass
+        self.yaw_controller = YawController(wheel_base, steer_ratio, MIN_SPEED, max_lat_accel, max_steer_angle)
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+
+    def control(self, linear_velocity, angular_velocity, current_velocity, dbw_enabled):
+        """
+        :param linear_velocity:
+        :param angular_velocity:
+        :param current_velocity:
+        :return: throttle, brake, steer
+        """
+
+        steer = self.yaw_controller.get_steering(linear_velocity, angular_velocity, current_velocity)
+
+        return 1., 0., steer
