d584b97aa44e87196332e8ad2f93b4e1ea74dfb8
Andrew Garkavyi <andrew.gawy@gmail.com>
2019-01-31 23:26:10 -0800

    Added throttle controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,4 +1,6 @@
 from yaw_controller import YawController
+from pid import PID
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -10,6 +12,14 @@ class Controller(object):
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, MIN_SPEED, max_lat_accel, max_steer_angle)
 
+        kp = 0.3
+        ki = 0.2
+        kd = 0.
+        min_throttle = 0.
+        max_throttle = 0.2
+        self.throttle_controller = PID(kp, ki, kd, min_throttle, max_throttle)
+
+        self.last_sample_time = 0
 
     def control(self, linear_velocity, angular_velocity, current_velocity, dbw_enabled):
         """
@@ -19,6 +29,17 @@ class Controller(object):
         :return: throttle, brake, steer
         """
 
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+
         steer = self.yaw_controller.get_steering(linear_velocity, angular_velocity, current_velocity)
 
-        return 1., 0., steer
+        vel_error = linear_velocity - current_velocity
+        cur_time = rospy.get_time()
+        sample_time = cur_time - self.last_sample_time
+        self.last_sample_time = cur_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+
+        return throttle, 0., steer
